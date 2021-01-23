47ffbe735838d4f0b926ea78d2984a2d60725057
TrevMike <preince560m@gmail.com>
2021-01-23 04:22:29 -0600

    cleaned up jquery

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,14 +1,9 @@
+// the toggle function for the navBar
 const menu = $(".menuToggle");
 const button = $(".toggle");
 const xmenu = $(".xMenu");
-let bolean = true;
 
 const toggleMenu = () => {
-  console.log("popping off for 1st function");
-  bolean
-    ? (button[0].src = "./imgs/nav-hamburger-close.png")
-    : (button[0].src = "./imgs/nav-hamburger.png");
-  bolean = !bolean;
   menu.toggleClass("menu");
   button.toggleClass("menuToggle");
 };
